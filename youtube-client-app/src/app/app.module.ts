3ba5ff5bf3a7c409d4c0f86e19c7ac2f643c9022
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-03-19 21:31:18 +0300

    fix: fix import in app component

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 import HeaderComponent from './header/header/header.component';
-import { SearchResultsComponent } from './search/search-results/search-results.component';
+import SearchResultsComponent from './search/search-results/search-results.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, SearchResultsComponent],
