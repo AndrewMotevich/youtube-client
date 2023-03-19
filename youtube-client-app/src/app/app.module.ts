beb20704280a968e7a0c43f7a4605d879f995e3c
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-03-19 20:36:39 +0300

    feat: add EsLint with AirBnB and basic components

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import AppRoutingModule from './app-routing.module';
+import AppComponent from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, SearchItemComponent, SearchResultsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export default class AppModule {}
